9296d37929da6cb047dc5307dc707928b5f930d7
Arda66 <ardaege66@hotmail.com>
2022-11-30 01:35:20 +0300

    Defining the reducers for redux.

---
+++ redux/reducer.js
@@ -0,0 +1,67 @@
+const INITIAL_STATE = {
+  Sessions: [],
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'CREATE_SESSION':
+      return {
+        ...state,
+        Sessions: [
+          ...state.Sessions,
+          {
+            id: state.Sessions.length + 1,
+            Photos: [],
+          },
+        ],
+      };
+    case 'ADD_PHOTO':
+      return {
+        ...state,
+        Sessions: [
+          ...state.Sessions,
+          //map through sessions and add photo to the selected session
+          state.Sessions.map(session => {
+            if (session.id === action.payload.id) {
+              // we need to find the session id that we pressed on and add the photo to it
+              return {
+                ...session,
+                Photos: [...session.Photos, action.payload.photo],
+              };
+            }
+            return session;
+          }),
+        ],
+      };
+    case 'DEL_PHOTO':
+      return {
+        ...state,
+        Sessions: [
+          ...state.Sessions,
+          //map through sessions and delete photo from the selected session
+          state.Sessions.map(session => {
+            if (session.id === action.payload.id) {
+              // we need to find the session id that we pressed on and delete the photo from it
+              return {
+                ...session,
+                Photos: session.Photos.filter(
+                  photo => photo !== action.payload.photo,
+                ),
+              };
+            }
+            return session;
+          }),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+// Session.push({
+//     id: 1,
+//     Photos: [],
+//     Date: new Date(),
+// });
+
+export default reducer;
