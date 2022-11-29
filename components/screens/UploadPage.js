94b4576ba95b7527173c13b3fc6a93aa7b1e50cc
Arda66 <ardaege66@hotmail.com>
2022-11-29 17:32:56 +0300

    Initialize screens

---
+++ components/screens/UploadPage.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const UploadPage = () => {
+  return (
+    <View>
+      <Text>UploadPage</Text>
+    </View>
+  );
+};
+
+export default UploadPage;
+
+const styles = StyleSheet.create({});
