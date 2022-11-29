6ae61c56610839ba34af3253e21b90e43474491b
Arda66 <ardaege66@hotmail.com>
2022-11-29 22:14:59 +0300

    UploadPage UI started to design.

--- components/screens/UploadPage.js
+++ components/screens/UploadPage.js
@@ -1,14 +1,66 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const UploadPage = () => {
+const UploadPage = ({navigation}) => {
   return (
-    <View>
-      <Text>UploadPage</Text>
+    <View style={styles.container}>
+      {/* Title */}
+      <Text style={styles.Title}>Uploaded Documents</Text>
+
+      {/* Uploaded Photos */}
+
+      {/* Add Photo Button */}
+      <TouchableOpacity style={styles.Button}>
+        <MaterialIcons name="add-a-photo" size={25} color="black" />
+        <Text style={styles.ButtonText}>Add Photo</Text>
+      </TouchableOpacity>
+      {/* Complete Session Button */}
+      <TouchableOpacity
+        onPress={() => navigation.navigate('MainPage')}
+        style={[
+          styles.Button,
+          {
+            width: '50%',
+            marginTop: 70,
+            justifyContent: 'space-around',
+            height: 50,
+          },
+        ]}>
+        <MaterialIcons name="check" size={25} color="black" />
+        <Text style={styles.ButtonText}>Complete Session</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default UploadPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  Title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+    color: 'black',
+  },
+  Button: {
+    alignSelf: 'center',
+    height: 150,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '80%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ButtonText: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'bold',
+    marginLeft: 15,
+  },
+});
