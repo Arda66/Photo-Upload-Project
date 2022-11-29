9c089a3e0f127b0285d2eedcb572ea3dc86ed87c
Arda66 <ardaege66@hotmail.com>
2022-11-29 17:33:33 +0300

    Setting the React-Navigation

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainPage from './components/screens/MainPage';
+import UploadPage from './components/screens/UploadPage';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="MainPage"
+          component={MainPage}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="UploadPage"
+          component={UploadPage}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
