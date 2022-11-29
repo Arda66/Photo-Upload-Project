c58188eeca85c5b5e0cddbfefd6d853ab848462f
Arda66 <ardaege66@hotmail.com>
2022-11-29 22:11:53 +0300

    Redux Provider is implemented.

--- App.js
+++ App.js
@@ -2,25 +2,29 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainPage from './components/screens/MainPage';
 import UploadPage from './components/screens/UploadPage';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="MainPage"
-          component={MainPage}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="UploadPage"
-          component={UploadPage}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            // options={{headerShown: false}}
+            name="MainPage"
+            component={MainPage}
+          />
+          <Stack.Screen
+            // options={{headerShown: false}}
+            name="UploadPage"
+            component={UploadPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
