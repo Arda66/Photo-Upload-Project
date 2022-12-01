580cf55ec11de1854a72c269de7e6e219e25653d
Arda66 <ardaege66@hotmail.com>
2022-12-01 03:56:11 +0300

    Updated the paths.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainPage from './src/screens/MainPage';
 import UploadPage from './src/screens/UploadPage';
-import store from './redux/store';
+import store from './src/redux/store';
 import {Provider} from 'react-redux';
 
 const App = () => {
